94ed090f0f2c27c4e66a151cf5af2743701688b4
wangbing11 <wangbing11@baidu.com>
2019-03-13 21:12:14 +0800

    add methed for user repositry

--- src/main/java/app/user/UserRepository.java
+++ src/main/java/app/user/UserRepository.java
@@ -1,7 +1,7 @@
 package app.user;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,8 +10,8 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends MongoRepository<User, String> {
 
-    User findBy_id(@Param("_id") ObjectId _id);
-
     List<User> findByNickname(@Param("nickname") String nickname);
+
+    List<User> findByCountryId(@Param("countryId") int countryId);
 
 }
